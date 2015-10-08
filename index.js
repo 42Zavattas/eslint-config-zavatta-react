a0819c350a46d63aa839950946747a04bbd94904
meriadec <meriadec.pillet@gmail.com>
2015-10-08 12:00:28 +0200

    fix(rule): remove strange rule

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = {
     'react/no-did-mount-set-state': 2,
     'react/no-did-update-set-state': 2,
     'react/no-direct-mutation-state': 2,
-    'react/no-multi-comp': 2,
+    'react/no-multi-comp': 0,
     'react/no-set-state': 0,
     'react/no-unknown-property': 2,
     'react/prop-types': 0,
